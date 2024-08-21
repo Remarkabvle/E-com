cdab49298056a41d338ec94cea3fb95959545273
Abubakr <abubakrdev69@gmail.com>
2024-08-21 11:08:36 +0500

    all changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Wishlist from './pages/wishlist/Wishlist';
-import SiteHeader from './components/header/Header';
 import Footer from './components/footer/Footer';
+import SiteHeader from './components/header/Header';
 import { WishlistProvider } from './context/WishlistContext';
 import { CartProvider } from './context/CartContext';
 import ProductDetail from './components/product/ProductDetail';
 import Cart from './pages/cart/Cart';
 import Login from './pages/login/Login';
-import Admin from './pages/admin/Admin'; // Import the Admin component
+import Admin from './pages/admin/Admin'; 
 
 function App() {
   return (
@@ -23,7 +23,7 @@ function App() {
             <Route path="/cart" element={<Cart />} />
             <Route path="/login" element={<Login />} />
             <Route path="/product/:id" element={<ProductDetail />} />
-            <Route path="/admin" element={<Admin />} /> {/* Add admin route */}
+            <Route path="/admin" element={<Admin />} />
           </Routes>
           <Footer />
         </Router>
